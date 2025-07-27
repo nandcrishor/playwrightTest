0727cac71663a1b6e501248f32b69babc022a33c
Nandakishor <nandakishor.j@calsoftinc.com>
2025-07-27 14:44:01 +0530

    removed duplicate test

--- tests/PracticalTaskTwo.spec.js
+++ tests/PracticalTaskTwo.spec.js
@@ -35,35 +35,3 @@ test("Fill Practice Form with Dynamic Data", async ({ page }) => {
   });
 });
 
-test("123Fill Practice Form with Dynamic Data", async ({ page }) => {
-  const form = new PracticalTaskTwo(page);
-  const userData = await generateTestData();
-  await form.navigate();
-  // await page.pause();                                                                                                              page.pause();
-
-  await form.clickOnAddButton();
-  await form.fillForm(
-    userData.firstName,
-    userData.lastName,
-    userData.email,
-    userData.age,
-    userData.salary,
-    userData.dept
-  );
-
-  const expectedValues = [
-    userData.firstName,
-    userData.lastName,
-    userData.age,
-    userData.email,
-    userData.salary,
-    userData.dept,
-  ];
-
-  const cellValues = await form.searchAndGetCellTextContents(userData.email);
-
-  expectedValues.forEach((expected, index) => {
-    console.log(expected);
-    expect(cellValues[index].trim()).toBe(expected);
-  });
-});
